a92d988340a2e6d07fcc22a49dce04b746450b64
Attila Bagossy <bagossyattila@outlook.com>
2019-04-28 00:29:37 +0200

    Documentation for SecurityLevel

--- include/identity-based/SecurityLevel.h
+++ include/identity-based/SecurityLevel.h
@@ -2,6 +2,14 @@
 #define __CRYPTID_SECURITY_LEVEL_H
 
 
+// References
+//  * [RFC-5091] Xavier Boyen, Luther Martin. 2007. RFC 5091. Identity-Based Cryptography Standard (IBCS) #1: Supersingular Curve Implementations of the BF and BB1 Cryptosystems
+
+
+/**
+ * The security level of the encryption. Please refer to Section 9. in [RFC-5091] regarding the meaning
+ * of the security levels.
+ */
 typedef enum
 {
     LOWEST,
