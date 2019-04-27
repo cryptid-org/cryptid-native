27c2bd6e9ba25280e1f8c2cf45b785c5a28194d0
Attila Bagossy <bagossyattila@outlook.com>
2019-04-27 23:33:44 +0200

    Documentation fixes in Divisor

--- include/elliptic/Divisor.h
+++ include/elliptic/Divisor.h
@@ -19,23 +19,24 @@ Complex divisor_evaluateVertical(const EllipticCurve ec, const AffinePoint a, co
 
 /**
  * Evaluates the divisor of a tangent on a Type-1 elliptic curve.
+ * @param result pointer to an element of \f$F_p^2\f$ that is the divisor of the line tangent to \f$A\f$ 
+ *               evaluated at \f$B\f$
  * @param ec the elliptic curve to operate on
  * @param a a point in \f$E(F_p)\f$
  * @param b a point \f$E(F_p^2)\f$
- * @param result pointer to an element of \f$F_p^2\f$ that is the divisor of the line tangent to \f$A\f$ 
- *               evaluated at \f$B\f$
- * @result SUCCESS if everything went right, DIVISOR_OF_TANGENT_INFINITY_ERROR if \f$B\f$ is the infinity point
+ * @result SUCCESS if everything went right
  */
 Status divisor_evaluateTangent(Complex* result, const EllipticCurve ec, const AffinePoint a, const ComplexAffinePoint b);
 
 /**
  * Evaluates the divisor of a line on a Type-1 elliptic curve.
+ * @param result pointer an element of \f$F_p^2\f$ that is the divisor of the line going through \f$A^{\prime}\f$ and 
+ *               \f$A^{\prime\prime}\f$ evaluated at \f$B\f$.
  * @param ec the elliptic curve to operate on
  * @param a a point in \f$E(F_p)\f$
  * @param aprime a point in \f$E(F_o)\f$
  * @param b a point in \f$E(F_p^2)\f$
- * @return an element of \f$F_p^2\f$ that is the divisor of the line going through \f$A^{\prime}\f$ and 
- *         \f$A^{\prime\prime}\f$ evaluated at \f$B\f$.
+ * @return SUCCESS if everything went right
  */
 Status divisor_evaluateLine(Complex* result, const EllipticCurve ec, const AffinePoint a, const AffinePoint aprime,
                             const ComplexAffinePoint b);
