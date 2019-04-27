98e3641a9d837606f3193b424f15b2e2e560a8aa
Attila Bagossy <bagossyattila@outlook.com>
2019-04-27 22:46:04 +0200

    Polished documentation for AffinePoint

--- include/elliptic/AffinePoint.h
+++ include/elliptic/AffinePoint.h
@@ -67,6 +67,13 @@ int affine_isEquals(const AffinePoint affinePoint1, const AffinePoint affinePoin
  */
 int affine_isInfinity(const AffinePoint affinePoint);
 
+/**
+ * Doubles (adds to itself) the specified AffinePoint.
+ * @param result the result of the operation
+ * @param affinePoint the point to double
+ * @param ellipticCurve the elliptic curve to operate over
+ * @result SUCCESS if everything went right, error otherwise
+ */
 Status affine_double(AffinePoint *result, const AffinePoint affinePoint, const EllipticCurve ellipticCurve);
 
 /**
@@ -76,8 +83,8 @@ Status affine_double(AffinePoint *result, const AffinePoint affinePoint, const E
  * @param result the result of the addition
  * @param affinePoint1 an AffinePoint
  * @param affinePoint2 an AffinePoint
- * @param ec the curve to operate over
- * @return 0 if everything went right, error otherwise
+ * @param ellipticCurve the curve to operate over
+ * @return SUCCESS if everything went right, error otherwise
  */
 Status affine_add(AffinePoint *result, const AffinePoint affinePoint1, const AffinePoint affinePoint2, const EllipticCurve ellipticCurve);
 
@@ -86,8 +93,8 @@ Status affine_add(AffinePoint *result, const AffinePoint affinePoint1, const Aff
  * @param result the result of the multiplication
  * @param s the scalar to multiply with
  * @param affinePoint the point to multiply
- * @param ec the elliptic curve to operate over
- * @return 0 if everything went right, error otherwise
+ * @param ellipticCurve the elliptic curve to operate over
+ * @return SUCCESS if everything went right, error otherwise
  */
 Status affine_multiply(AffinePoint *result, const mpz_t s, const AffinePoint affinePoint, const EllipticCurve ellipticCurve);
 
