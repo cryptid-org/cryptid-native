0b0dbcbc3658596068ea9b4f2024a8aa4af35961
Attila Bagossy <bagossyattila@outlook.com>
2019-04-27 23:29:41 +0200

    Documentation and const correctness in Divisor

--- include/elliptic/Divisor.h
+++ include/elliptic/Divisor.h
@@ -8,10 +8,36 @@
 #include "util/Status.h"
 
 
-Complex divisor_evaluateVertical(EllipticCurve ec, AffinePoint a, ComplexAffinePoint b);
+/**
+ * Evaluates the divisor of a vertical line on a Type-1 elliptic curve.
+ * @param ec the elliptic curve to operate on
+ * @param a a point in \f$E(F_p)\f$
+ * @param b a point \f$E(F_p^2)\f$
+ * @return an element of \f$F_p^2\f$ that is the divisor of the vertical line going through \f$A\f$ evaluated at \f$B\f$
+ */
+Complex divisor_evaluateVertical(const EllipticCurve ec, const AffinePoint a, const ComplexAffinePoint b);
 
-Status divisor_evaluateTangent(Complex* result, EllipticCurve ec, AffinePoint a, ComplexAffinePoint b);
+/**
+ * Evaluates the divisor of a tangent on a Type-1 elliptic curve.
+ * @param ec the elliptic curve to operate on
+ * @param a a point in \f$E(F_p)\f$
+ * @param b a point \f$E(F_p^2)\f$
+ * @param result pointer to an element of \f$F_p^2\f$ that is the divisor of the line tangent to \f$A\f$ 
+ *               evaluated at \f$B\f$
+ * @result SUCCESS if everything went right, DIVISOR_OF_TANGENT_INFINITY_ERROR if \f$B\f$ is the infinity point
+ */
+Status divisor_evaluateTangent(Complex* result, const EllipticCurve ec, const AffinePoint a, const ComplexAffinePoint b);
 
-Status divisor_evaluateLine(Complex* result, EllipticCurve ec, AffinePoint a, AffinePoint aprime, ComplexAffinePoint b);
+/**
+ * Evaluates the divisor of a line on a Type-1 elliptic curve.
+ * @param ec the elliptic curve to operate on
+ * @param a a point in \f$E(F_p)\f$
+ * @param aprime a point in \f$E(F_o)\f$
+ * @param b a point in \f$E(F_p^2)\f$
+ * @return an element of \f$F_p^2\f$ that is the divisor of the line going through \f$A^{\prime}\f$ and 
+ *         \f$A^{\prime\prime}\f$ evaluated at \f$B\f$.
+ */
+Status divisor_evaluateLine(Complex* result, const EllipticCurve ec, const AffinePoint a, const AffinePoint aprime,
+                            const ComplexAffinePoint b);
 
 #endif
--- src/elliptic/Divisor.c
+++ src/elliptic/Divisor.c
@@ -3,8 +3,16 @@
 #include "elliptic/Divisor.h"
 
 
-Complex divisor_evaluateVertical(EllipticCurve ec, AffinePoint a, ComplexAffinePoint b)
+// References
+//  * [RFC-5091] Xavier Boyen, Luther Martin. 2007. RFC 5091. Identity-Based Cryptography Standard (IBCS) #1: Supersingular Curve Implementations of the BF and BB1 Cryptosystems
+
+
+Complex divisor_evaluateVertical(const EllipticCurve ec, const AffinePoint a, const ComplexAffinePoint b)
 {
+    // Implementation of Algorithm 3.4.1 in [RFC-5091].
+
+    // Let \f$r\f$ denote the result of the operation:
+    // \f$r = x_B - x_A\f$
     Complex result;
 
     if(affine_isInfinity(a))
@@ -25,13 +33,17 @@ Complex divisor_evaluateVertical(EllipticCurve ec, AffinePoint a, ComplexAffineP
     return result;
 }
 
-Status divisor_evaluateTangent(Complex* result, EllipticCurve ec, AffinePoint a, ComplexAffinePoint b)
+Status divisor_evaluateTangent(Complex* result, const EllipticCurve ec, const AffinePoint a, const ComplexAffinePoint b)
 {
+    // Implementation of Algorithm 3.4.2 in [RFC-5091].
+
+    // Argument check
     if(complexAffine_isInfinity(b))
     {
         return DIVISOR_OF_TANGENT_INFINITY_ERROR;
     }
 
+    // Special cases
     if(affine_isInfinity(a))
     {
         *result = complex_initLong(1, 0);
@@ -48,15 +60,19 @@ Status divisor_evaluateTangent(Complex* result, EllipticCurve ec, AffinePoint a,
     mpz_t threeAddInv, minusThree, xasquared, aprime, bprime, bAddInv, bAddInvyA, axA, axAaddInv, c;
     mpz_inits(threeAddInv, minusThree, xasquared, aprime, bprime, bAddInv, bAddInvyA, axA, axAaddInv, c, NULL);
 
+    // Line computation
+    // \f$a^{\prime} = -3 \cdot x_A^2\f$
     mpz_set_si(minusThree, -3);
     mpz_mod(threeAddInv, minusThree, ec.fieldOrder);
     mpz_powm_ui(xasquared, a.x, 2, ec.fieldOrder);
     mpz_mul(aprime, xasquared, threeAddInv);
     mpz_mod(aprime, aprime, ec.fieldOrder);
 
+    // \f$b^{\prime} = 2 \cdot y_A\f$
     mpz_mul_ui(bprime, a.y, 2);
     mpz_mod(bprime, bprime, ec.fieldOrder);
 
+    // \f$c = -b^{\prime} \cdot y_A - a^{\prime} \cdot x_A\f$
     mpz_neg(bAddInv, bprime);
     mpz_mod(bAddInv, bAddInv, ec.fieldOrder);
     mpz_mul(bAddInvyA, bAddInv, a.y);
@@ -68,6 +84,9 @@ Status divisor_evaluateTangent(Complex* result, EllipticCurve ec, AffinePoint a,
     mpz_add(c, bAddInvyA, axAaddInv);
     mpz_mod(c, c, ec.fieldOrder);
 
+    // Evaluation at \f$B\f$
+    // Let \f$r\f$ denote the result:
+    // \f$r = a^{\prime} \cdot x_B + b^{\prime} \cdot y_B + c\f$
     axB = complex_modMulScalar(b.x, aprime, ec.fieldOrder);
     byB = complex_modMulScalar(b.y, bprime, ec.fieldOrder);
     resultPart = complex_modAdd(axB, byB, ec.fieldOrder);
@@ -78,13 +97,18 @@ Status divisor_evaluateTangent(Complex* result, EllipticCurve ec, AffinePoint a,
     return SUCCESS;
 }
 
-Status divisor_evaluateLine(Complex* result, EllipticCurve ec, AffinePoint a, AffinePoint aprime, ComplexAffinePoint b)
+Status divisor_evaluateLine(Complex* result, const EllipticCurve ec, const AffinePoint a, const AffinePoint aprime, 
+                            const ComplexAffinePoint b)
 {
+    // Implementation of Algorithm 3.4.3 in [RFC-5091].
+
+    // Argument check
     if(complexAffine_isInfinity(b))
     {
         return DIVISOR_OF_LINE_INFINITY_ERROR;
     }
 
+    // Special cases
     if(affine_isInfinity(a))
     {
         *result = divisor_evaluateVertical(ec, aprime, b);
@@ -115,12 +139,16 @@ Status divisor_evaluateLine(Complex* result, EllipticCurve ec, AffinePoint a, Af
     mpz_inits(linea, lineb, linebaddinv, q, t, taddinv, linec, NULL);
     Complex axb, byb, resultPart;
 
+    // Line computation
+    // \f$a = y_A^{\prime} - y_A^{\prime\prime}\f$
     mpz_sub(linea, a.y, aprime.y);
     mpz_mod(linea, linea, ec.fieldOrder);
 
+    // \f$b = x_A^{\prime\prime} - x_A^{\prime}\f$
     mpz_sub(lineb, aprime.x, a.x);
     mpz_mod(lineb, lineb, ec.fieldOrder);
 
+    // \f$c = -b \cdot y_A^{\prime} - a \cdot x_A^{\prime}\f$
     mpz_neg(linebaddinv, lineb);
     mpz_mod(linebaddinv, linebaddinv, ec.fieldOrder);
     mpz_mul(q, linebaddinv, a.y);
@@ -132,6 +160,9 @@ Status divisor_evaluateLine(Complex* result, EllipticCurve ec, AffinePoint a, Af
     mpz_add(linec, q, taddinv);
     mpz_mod(linec, linec, ec.fieldOrder);
 
+    // Evaluation at B
+    // Let \f$r\f$ denote the result:
+    // \f$r = a \cdot x_B + b \cdot y_B + c\f$
     axb = complex_modMulScalar(b.x, linea, ec.fieldOrder);
     byb = complex_modMulScalar(b.y, lineb, ec.fieldOrder);
     resultPart = complex_modAddScalar(byb, linec, ec.fieldOrder);
