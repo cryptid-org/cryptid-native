1ff6d8a08a374d5eb59d839ea24dd08aa35409fa
Attila Bagossy <bagossyattila@outlook.com>
2019-04-28 00:08:18 +0200

    Documentation and const correctness in CipherTextTuple

--- include/identity-based/CipherTextTuple.h
+++ include/identity-based/CipherTextTuple.h
@@ -6,17 +6,51 @@
 #include "elliptic/AffinePoint.h"
 
 
+// References
+//  * [RFC-5091] Xavier Boyen, Luther Martin. 2007. RFC 5091. Identity-Based Cryptography Standard (IBCS) #1: Supersingular Curve Implementations of the BF and BB1 Cryptosystems
+
+
+/**
+ * Struct holding the ciphertext data. Refer to {@code BFCiphertextBlock} in [RFC-5091].
+ */
 typedef struct CipherTextTuple
 {
+    /**
+     * A point in \f$E(F_p)\f$.
+     */
     AffinePoint cipherU;
+
+    /**
+     * A binary string.
+     */
     unsigned char* cipherV;
     size_t cipherVLength;
+
+    /**
+     * A binary string.
+     */
     unsigned char* cipherW;
     size_t cipherWLength;
 } CipherTextTuple;
 
-CipherTextTuple cipherTextTuple_init(AffinePoint cipherU, unsigned char* cipherV, int cipherVLength, unsigned char* cipherW, int cipherWLength);
 
+/**
+ * Returns a new CipherTextTuple initialized with the specified values. Note, that {@code cipherV} and {@code cipherw}
+ * will be copied.
+ * @param cipherU an AffinePointer
+ * @param cipherV binary string
+ * @param cipherVLength the size of {@code cipherV}
+ * @param cipherW binary string
+ * @param cipherWLength the size of {@code cipherW}
+ * @return a new CipherTextTuple instance
+ */
+CipherTextTuple cipherTextTuple_init(const AffinePoint cipherU, const unsigned char *const cipherV, const int cipherVLength,
+                                     const unsigned char *const cipherW, const int cipherWLength);
+
+/**
+ * Frees a CipherTextTuple.
+ * @param cipherTextTuple the CipherTextTuple to be destroyed
+ */
 void cipherTextTuple_destroy(CipherTextTuple cipherTextTuple);
 
 #endif
--- src/identity-based/CipherTextTuple.c
+++ src/identity-based/CipherTextTuple.c
@@ -4,17 +4,18 @@
 #include "identity-based/CipherTextTuple.h"
 
 
-CipherTextTuple cipherTextTuple_init(AffinePoint cipherU, unsigned char* cipherV, int cipherVLength, unsigned char* cipherW, int cipherWLength)
-{    
+CipherTextTuple cipherTextTuple_init(const AffinePoint cipherU, const unsigned char *const cipherV, const int cipherVLength,
+                                     const unsigned char *const cipherW, const int cipherWLength)
+{
     CipherTextTuple cipherTextTuple;
     cipherTextTuple.cipherU = affine_init(cipherU.x, cipherU.y);
     
-    cipherTextTuple.cipherV = (unsigned char*)malloc(cipherVLength*sizeof(unsigned char) + 1);
+    cipherTextTuple.cipherV = (unsigned char*)malloc(cipherVLength * sizeof(unsigned char) + 1);
     memcpy(cipherTextTuple.cipherV, cipherV, cipherVLength + 1);
     
     cipherTextTuple.cipherVLength = cipherVLength;
     
-    cipherTextTuple.cipherW = (unsigned char*)malloc(cipherWLength*sizeof(unsigned char) + 1);
+    cipherTextTuple.cipherW = (unsigned char*)malloc(cipherWLength * sizeof(unsigned char) + 1);
     memcpy(cipherTextTuple.cipherW, cipherW, cipherWLength + 1);
     
     cipherTextTuple.cipherWLength = cipherWLength;
