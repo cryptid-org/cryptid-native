213a1750335d5d22112cf86d4994386a797e8c75
Attila Bagossy <bagossyattila@outlook.com>
2019-04-27 23:53:50 +0200

    Documentation and const correctness in TatePairing

--- include/elliptic/TatePairing.h
+++ include/elliptic/TatePairing.h
@@ -9,7 +9,17 @@
 #include "complex/Complex.h"
 #include "util/Status.h"
 
-
-Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve ellipticCurve, mpz_t subgroupOrder, AffinePoint p, AffinePoint b);
+/**
+ * Computes the Tate pairing over Type-1 elliptic curves.
+ * @param result pointer to the resulting Complex value
+ * @param embeddingDegree the embedding degree of the curve
+ * @param ellipticCurve the elliptic curve to operate on
+ * @param subgroupOrder the order of the subgroup
+ * @param p a point of \f$E[r]\f$
+ * @param b
+ * @return SUCCESS if everything went right
+ */
+Status tate_performPairing(Complex *result, const int embeddingDegree, const EllipticCurve ellipticCurve,
+                           const mpz_t subgroupOrder, const AffinePoint p, const AffinePoint b);
 
 #endif
--- src/elliptic/TatePairing.c
+++ src/elliptic/TatePairing.c
@@ -2,11 +2,24 @@
 #include "elliptic/TatePairing.h"
 
 
-Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve ellipticCurve, mpz_t subgroupOrder, AffinePoint p, AffinePoint b)
+// References:
+//   * [Intro-to-IBE] Luther Martin. 2008. Introduction to Identity-Based Encryption (Information Security and Privacy Series) (1 ed.). Artech House, Inc., Norwood, MA, USA. 
+
+
+Status tate_performPairing(Complex *result, const int embeddingDegree, const EllipticCurve ellipticCurve,
+                           const mpz_t subgroupOrder, const AffinePoint p, const AffinePoint b)
 {
+    // Implementation of Miller's algorithm as it's written on this page:
+    // https://crypto.stanford.edu/pbc/notes/ep/miller.html
     ComplexAffinePoint q;
 
-    //start of the distortion map
+    // Distortion map - Creates linearly independent points
+    // For examples on distortion maps, see [Intro-to-IBE p63.].
+    //
+    // Here we use a Xi distortion map, which involves calculating a \f$\xi\f$ value.
+    // For Type-1 elliptic curves, this is calculated as follows:
+    // \f$\xi = \frac{p - 1}{2}(1 + 3^{\frac{p + 1}{4}}i)\f$
+    // where \f$p\f$ is the field order of the elliptic curve field.
     if(affine_isInfinity(b))
     {
         q = complexAffine_infinity();
@@ -29,6 +42,11 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
         tmp = complex_initMpz(one, bxi);
         xi = complex_modMulScalar(tmp, axi, ellipticCurve.fieldOrder);
 
+        // \f$x^{\prime} = x \cdot xi\f$
+        // \f$x \in \f$F_p\f$ | \f$xi\f$ \in \f$F_p^2\f$
+        //
+        // Here we assume, that we have to convert \f$x\f$ to \f$F_p^2\f$ and then perform the multiplication
+        // according to the complex multiplication rules.
         xprime = complex_modMulScalar(xi, b.x, ellipticCurve.fieldOrder);
 
         mpz_t zero;
@@ -43,8 +61,7 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
         mpz_clears(axi, bxi, three, one, addition, quotient, difference, NULL);
         complex_destroyMany(3, xi, tmp, xprime);
     }
-    //end of the distortion map
-
+    
     if(complexAffine_isInfinity(q))
     {
         complexAffine_destroy(q);
@@ -52,16 +69,23 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
         return SUCCESS;
     }
 
-    //start of the miller algorithm
+    // Now p and q are linearly indenependent.
+    // Here we start the actual Miller's algorithm.
     Complex f, gVVQ, g2VMinus2VQ, g2VMinus2VQInv, frac, tmpF, gVPQ, gVPlusQ, gVPlusQInv;
     AffinePoint v, doubleV, vPlusP;
 
+    // 1. Set \f$f\f$ = 1 and \f$v\f$ = \f$p\f$
     f = complex_initLong(1, 0);
     v = affine_init(p.x, p.y);
     
+    // 2. {@code for i = t - 1 to 0 do:}
+    // where \f$t\f$ is the bitcount of the subgroup order.
+    // Note, that we have to subtract 2 because of the allocation behavior
+    // of GMP (please refer to https://gmplib.org/manual/Miscellaneous-Integer-Functions.html).
     for(int i = mpz_sizeinbase(subgroupOrder, 2) - 2; i >= 0; --i)
     {
-        //start of double step
+        // Double step
+        // \f$f = f^{2} \frac{g_{v, v}(q)}{g_{2v, -2v}(q)}\f$
         Status status = affine_add(&doubleV, v, v, ellipticCurve);
         if(status)
         {
@@ -96,15 +120,16 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
         f = complex_modMul(tmpF, frac, ellipticCurve.fieldOrder);
 
         affine_destroy(v);
+        // \f$v = 2v\f$
         v = affine_init(doubleV.x, doubleV.y);
 
         complex_destroyMany(5, gVVQ, g2VMinus2VQ, g2VMinus2VQInv, frac, tmpF);
         affine_destroy(doubleV);
-        //end of double step
-
+        
         if(mpz_tstbit(subgroupOrder, i))
         {
-            //start of add step
+            // Add step
+            // \f$f = f \frac{g_{v, p}(q)}{g_{v + p, -(b + p)}(q)}\f$
             status = affine_add(&vPlusP, v, p, ellipticCurve);
             if(status)
             {
@@ -113,6 +138,7 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
                 affine_destroy(v);
                 return status;
             }
+
             status = divisor_evaluateLine(&gVPQ, ellipticCurve, v, p, q);
             if(status)
             {
@@ -122,7 +148,9 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
                 affine_destroy(vPlusP);
                 return status;
             }
+
             gVPlusQ = divisor_evaluateVertical(ellipticCurve, vPlusP, q);
+
             status = complex_multiplicativeInverse(&gVPlusQInv, gVPlusQ, ellipticCurve.fieldOrder);
             if(status)
             {
@@ -139,18 +167,17 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
             f = complex_initMpz(tmpF.real, tmpF.imaginary);
 
             affine_destroy(v);
+            // \f$v = v + p\f$
             v = affine_init(vPlusP.x, vPlusP.y);
 
             complex_destroyMany(5, gVPQ, gVPlusQ, gVPlusQInv, frac, tmpF);
             affine_destroy(vPlusP);
-            //end of add step
         }
     }
     affine_destroy(v);
     complexAffine_destroy(q);
-    //end of the miller algorithm
 
-    //start of final exponentiation
+    // Final Exponentiation
     mpz_t exponent, pPow, exponentPart;
     mpz_inits(exponent, pPow, exponentPart, NULL);
 
@@ -162,7 +189,6 @@ Status tate_performPairing(Complex *result, int embeddingDegree, EllipticCurve e
 
     mpz_clears(exponent, pPow, exponentPart, NULL);
     complex_destroy(f);
-    //end of final exponentiation
 
     return SUCCESS;
 }
