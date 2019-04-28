1f3067ccf27432f1bebb5d6a0983451246051f49
Attila Bagossy <bagossyattila@outlook.com>
2019-04-28 10:45:20 +0200

    Documentation in CryptID

--- include/CryptID.h
+++ include/CryptID.h
@@ -10,12 +10,56 @@
 #include "util/Status.h"
 
 
-Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParameters, mpz_t masterSecret);
+/**
+ * Establishes a master secret and public parameters for a given security level. The master secret (as its name suggests)
+ * should be kept secret, while the public parameters can be distributed among the clients.
+ * @param securityLevel the desired security level
+ * @param publicParameters Pointer in which the public parameters will be stored. If the return value is SUCCESS
+ *                         then it will point to correctly filled PublicParameters instance. Note, that the
+ *                         q field must be mpz_init'd and thus mpz_clear'd be the caller.
+ * @param masterSecret Out parameter which will hold the master secret. Must be mpz_init'd and mpz_clear'd by the caller.
+ * @return SUCCESS if everything went right
+ */
+Status cryptid_setup(const SecurityLevel securityLevel, PublicParameters* publicParameters, mpz_t masterSecret);
 
-Status cryptid_extract(AffinePoint* result, char* identity, size_t identityLength, PublicParameters publicParameters, mpz_t masterSecret);
+/**
+ * Extracts the private key corresponding to a given identity string.
+ * @param result Out parameter holding the private key (which is an AffinePoint). On SUCCESS, it must be destroyed by 
+ *               the caller. Initialization is done by this function.
+ * @param identity the identity string we're extracting the private key for
+ * @param identityLength the length of the identity string
+ * @param publicParameters the IBE public parameters
+ * @param masterSecret the master secret corresponding to the public parameters
+ * @return SUCCESS if everything went right
+ */
+Status cryptid_extract(AffinePoint* result, const char *const identity, const size_t identityLength, 
+                       const PublicParameters publicParameters, const mpz_t masterSecret);
 
-Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLength, char* identity, size_t identityLength, PublicParameters publicParameters);
+/**
+ * Encrypts a message with the given identity string.
+ * @param result Out parameter storing the ciphertext. If the return value is SUCCESS
+ *               then it will point to an CipherTextTuple instance, that must be destroyed by the caller.
+ *               Initialization is done by this function.
+ * @param message the string to encrypt
+ * @param messageLength the length of the message
+ * @param identity the identity string to encrypt with
+ * @param identityLength the length of the identity
+ * @param publicParameters the IBE public parameters
+ * @return SUCCESS if everything went right
+ */
+Status cryptid_encrypt(CipherTextTuple *result, const char *const message, const size_t messageLength,
+                       const char *const identity, const size_t identityLength, const PublicParameters publicParameters);
 
-Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ciphertext, PublicParameters publicParameters);
+/**
+ * Decrypts a previously encrypted message using the specified private key.
+ * @param result Out parameter holding the message in plaintext. If the return value is SUCCESS
+ *               then it will point to a zero-terminated string, that must be destroyed by the caller. 
+ * @param privateKey the private key to decrypt with
+ * @param ciphertext the ciphertext to decrypt
+ * @param publicParameters the IBE public parameters
+ * @return SUCCESS of everything went right
+ */
+Status cryptid_decrypt(char **result, const AffinePoint privateKey, const CipherTextTuple ciphertext, 
+                       const PublicParameters publicParameters);
 
 #endif
--- src/CryptID.c
+++ src/CryptID.c
@@ -9,21 +9,30 @@
 #include "util/Validation.h"
 
 
+// References
+//  * [RFC-5091] Xavier Boyen, Luther Martin. 2007. RFC 5091. Identity-Based Cryptography Standard (IBCS) #1: Supersingular Curve Implementations of the BF and BB1 Cryptosystems
+
+
 static const unsigned int SOLINAS_GENERATION_ATTEMPT_LIMIT = 100;
 static const unsigned int POINT_GENERATION_ATTEMPT_LIMIT = 100;
 
 static const unsigned int Q_LENGTH_MAPPING[] = { 160, 224, 256, 384, 512 };
 static const unsigned int P_LENGTH_MAPPING[] = { 512, 1024, 1536, 3840, 7680 };
 
-Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParameters, mpz_t masterSecret)
+Status cryptid_setup(const SecurityLevel securityLevel, PublicParameters* publicParameters, mpz_t masterSecret)
 {
+    // Implementation of Algorithm 5.1.2 (BFsetup1) in [RFC-5091].
+    // Note, that instead of taking the bitlengts of p and q as arguments, this function takes
+    // a security level which is in turn translated to bitlengths using {@code P_LENGTH_MAPPING} and
+    // {@code Q_LENGTH_MAPPING}.
+
     if (!publicParameters)
     {
         return PUBLIC_PARAMETERS_NULL_ERROR;
     }
 
     // Construct the elliptic curve and its subgroup of interest
-    // Select a random n_q-bit Solinas prime q
+    // Select a random \f$n_q\f$-bit Solinas prime \f$q\f$.
     mpz_t q;
     mpz_init(q);
 
@@ -35,7 +44,7 @@ Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParame
         return status;
     }
 
-    // Select a random integer r, such that p = 12 * r * q - 1 is an n_p-bit prime
+    // Select a random integer \f$r\f$, such that \f$p = 12 \cdot r \cdot q - 1\f$ is an \f$n_p\f$-bit prime.
     unsigned int lengthOfR = P_LENGTH_MAPPING[(int) securityLevel] - Q_LENGTH_MAPPING[(int) securityLevel] - 3;
     mpz_t r, p;
     mpz_inits(r, p, NULL);
@@ -56,7 +65,7 @@ Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParame
 
     mpz_clears(zero, one, NULL);
 
-    // Select a point P of order q in E(F_p)
+    // Select a point \f$P\f$ of order \f$q\f$ in \f$E(F_p)\f$.
     AffinePoint pointP;
     do
     {
@@ -89,7 +98,7 @@ Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParame
     }
     while (affine_isInfinity(pointP));
 
-    // Determine the master secret
+    // Determine the master secret.
     mpz_t qMinusTwo, s;
     mpz_init(s);
     mpz_init_set(qMinusTwo, q);
@@ -99,7 +108,7 @@ Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParame
     mpz_add_ui(s, s, 2);
     mpz_clear(qMinusTwo);
 
-    // Determine the public parameters
+    // Determine the public parameters.
     AffinePoint pointPpublic;
 
     status = affine_wNAFMultiply(&pointPpublic, s, pointP, ec);
@@ -113,6 +122,7 @@ Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParame
     }
 
     publicParameters->ellipticCurve = ec;
+    // TODO: Also init to better conform with other functions.
     mpz_set(publicParameters->q, q);
     publicParameters->pointP = pointP;
     publicParameters->pointPpublic = pointPpublic;
@@ -125,8 +135,11 @@ Status cryptid_setup(SecurityLevel securityLevel, PublicParameters* publicParame
     return SUCCESS;
 }
 
-Status cryptid_extract(AffinePoint* result, char* identity, size_t identityLength, PublicParameters publicParameters, mpz_t masterSecret)
+Status cryptid_extract(AffinePoint* result, const char *const identity, const size_t identityLength, 
+                       const PublicParameters publicParameters, const mpz_t masterSecret)
 {
+    // Implementation of Algorithm 5.3.1 (BFextractPriv) in [RFC-5091].
+
     if (!result)
     {
         return RESULT_POINTER_NULL_ERROR;
@@ -144,6 +157,7 @@ Status cryptid_extract(AffinePoint* result, char* identity, size_t identityLengt
 
     AffinePoint qId;
 
+    // Let \f$Q_{id} = \mathrm{HashToPoint}(E, p, q, id, \mathrm{hashfcn})\f$.
     Status status =
         hashToPoint(&qId, publicParameters.ellipticCurve, publicParameters.ellipticCurve.fieldOrder, publicParameters.q, identity, identityLength, publicParameters.hashFunction);
 
@@ -152,6 +166,7 @@ Status cryptid_extract(AffinePoint* result, char* identity, size_t identityLengt
         return status;
     }
 
+    // Let \f$S_{id} = [s]Q_{id}\f$.
     status = affine_wNAFMultiply(result, masterSecret, qId, publicParameters.ellipticCurve);
 
     affine_destroy(qId);
@@ -159,8 +174,11 @@ Status cryptid_extract(AffinePoint* result, char* identity, size_t identityLengt
     return status;
 }
 
-Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLength, char* identity, size_t identityLength, PublicParameters publicParameters)
+Status cryptid_encrypt(CipherTextTuple *result, const char *const message, const size_t messageLength,
+                       const char *const identity, const size_t identityLength, const PublicParameters publicParameters)
 {
+    // Implementation of Algorithm 5.4.1 (BFencrypt) in [RFC-5091].
+
     if(!message)
     {
         return MESSAGE_NULL_ERROR;
@@ -189,8 +207,12 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
     mpz_t l;
     mpz_init(l);
 
+    // Let {@code hashlen} be the length of the output of the cryptographic hash
+    // function hashfcn from the public parameters.
     int hashLen = publicParameters.hashFunction.hashLength;
 
+    // \f$Q_{id} = \mathrm{HashToPoint}(E, p, q, id, \mathrm{hashfcn})\f$
+    // which results in a point of order \f$q\f$ in \f$E(F_p)\f$.
     AffinePoint pointQId;
     Status status = hashToPoint(&pointQId, publicParameters.ellipticCurve, publicParameters.ellipticCurve.fieldOrder, publicParameters.q, identity, identityLength, publicParameters.hashFunction);
     if(status)
@@ -199,12 +221,19 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
         return status;
     }
 
+    // Select a random {@code hashlen}-bit vector {@code rho}, represented as (\f$\frac{\mathrm{hashlen}}{8}\f$)-octet
+    // string in big-endian convention.
     unsigned char* rho = (unsigned char*)calloc(hashLen + 1, sizeof(unsigned char));
     randomBytes(rho, hashLen);
     rho[hashLen] = '\0';
 
+    // Let \f$t = \mathrm{hashfcn}(m)\f$, a {@code hashlen}-octet string resulting from applying
+    // the {@code hashfcn} algorithm to the input \f$m\f$.
     unsigned char* t = (*(publicParameters.hashFunction.sha_hash))((unsigned char*) message, messageLength, NULL);
 
+    // Let \f$l = \mathrm{HashToRange}(rho || t, q, \mathrm{hashfcn})\f$, an integer in the range
+    // \f$0\f$ to \f$q - 1\f$ resulting from applying {@code HashToRange}
+    // to the \f$(2 * \mathrm{hashlen})\f$-octet concatenation of {@code rho} and \f$t\f$.
     unsigned char* concat = (unsigned char*)calloc(2*hashLen + 1, sizeof(unsigned char));
     for(int i = 0; i < hashLen; i++)
     {
@@ -218,6 +247,7 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
 
     hashToRange(l, concat, 2*hashLen, publicParameters.q, publicParameters.hashFunction);
 
+    // Let \f$U = [l]P\f$, which is a point of order \f$q\f$ in \f$E(F_p)\f$.
     AffinePoint cipherPointU;
     status = affine_wNAFMultiply(&cipherPointU, l, publicParameters.pointP, publicParameters.ellipticCurve);
     if(status)
@@ -230,6 +260,8 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
         return status;
     }
     
+    // Let \f$\mathrm{theta} = \mathrm{Pairing}(E, p, q, P_{pub}, Q_{id})\f$, which is an element of
+    // the extension field \f$F_p^2\f$ obtained using the modified Tate pairing.
     Complex theta;
     status = tate_performPairing(&theta, 2, publicParameters.ellipticCurve, publicParameters.q, publicParameters.pointPpublic, pointQId);
     if(status)
@@ -243,13 +275,20 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
         return status;
     }
 
+    //Let \f$\mathrm{theta}^{\prime} = \mathrm{theta}^l\f$, which is theta raised to the power of \f$l\f$ in \f$F_p^2\f$.
     Complex thetaPrime = complex_modPow(theta, l, publicParameters.ellipticCurve.fieldOrder);
 
+    // Let \f$z = \mathrm{Canonical}(p, k, 0, \mathrm{theta}^{\prime})\f$, a canonical string
+    // representation of {@code theta'}.
     int zLength;
     unsigned char* z = canonical(&zLength, publicParameters.ellipticCurve.fieldOrder, thetaPrime, 1);
 
+    // Let \f$w = \mathrm{hashfcn}(z)\f$ using the {@code hashfcn} hashing algorithm, the
+    // result of which is a {@code hashlen}-octet string.
     unsigned char* w = (*(publicParameters.hashFunction.sha_hash))(z, zLength, NULL);
 
+    // Let \f$V = w \oplus rho\f$, which is the {@code hashlen}-octet long bit-wise XOR
+    // of \f$w\f$ and {@code rho}.
     unsigned char* cipherV = (unsigned char*)calloc(hashLen + 1, sizeof(unsigned char));
     for(int i = 0; i < hashLen; i++) 
     {
@@ -257,6 +296,9 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
     }
     cipherV[hashLen] = '\0';
 
+    // Let W = \mathrm{HashBytes}(|m|, rho, \mathrm{hashfcn}) \oplus m\f$, which is the bit-wise
+    // XOR of \f$m\f$ with the first \f$|m|\f$ octets of the pseudo-random bytes
+    // produced by {@code HashBytes} with seed {@code rho}.
     unsigned char* cipherW = (unsigned char*)calloc(messageLength + 1, sizeof(unsigned char));
     unsigned char* hashedBytes = hashBytes(messageLength, rho, hashLen, publicParameters.hashFunction);
     for(int i = 0; i < messageLength; i++) 
@@ -265,6 +307,7 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
     }
     cipherW[messageLength] = '\0';
 
+    // The ciphertext is the triple \f$(U, V, W)\f$.
     *result = cipherTextTuple_init(cipherPointU, cipherV, hashLen, cipherW, messageLength);
 
     mpz_clear(l);
@@ -281,8 +324,11 @@ Status cryptid_encrypt(CipherTextTuple *result, char* message, size_t messageLen
     return SUCCESS;
 }
 
-Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ciphertext, PublicParameters publicParameters)
+Status cryptid_decrypt(char **result, const AffinePoint privateKey, const CipherTextTuple ciphertext, 
+                       const PublicParameters publicParameters)
 {
+    // Implementation of Algorithm 5.5.1 (BFdecrypt) in [RFC-5091].
+
     if(!validation_isPublicParametersValid(publicParameters))
     {
         return ILLEGAL_PUBLIC_PARAMETERS_ERROR;
@@ -301,8 +347,12 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
     mpz_t l;
     mpz_init(l);
 
+    // Let {@code hashlen} be the length of the output of the hash function
+    // {@code hashfcn} measured in octets.
     int hashLen = publicParameters.hashFunction.hashLength;
 
+    // Let \f$theta = \mathrm{Pairing}(E, p ,q, U, S_{id})\f$ by applying the modified
+    // Tate pairing.
     Complex theta;
     Status status = tate_performPairing(&theta, 2, publicParameters.ellipticCurve, publicParameters.q, ciphertext.cipherU, privateKey);
     if(status)
@@ -311,11 +361,15 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
         return status;
     }
     
+    // Let \f$z = \mathrm{Canonical}(p, k, 0, theta)\f$ a canonical string representation of {@code theta}.
     int zLength;
     unsigned char* z = canonical(&zLength, publicParameters.ellipticCurve.fieldOrder, theta, 1);
 
+    // Let \f$w = \mathrm{hashfcn}(z)$ using the {@code hashfcn} hashing algorithm, the result
+    // of which is a {@code hashlen}-octet string.
     unsigned char* w = (*(publicParameters.hashFunction.sha_hash))(z, zLength, NULL);
 
+    // Let \f$rho = w \oplus V\f$, the bit-wise XOR of \f$w\f$ and \f$V\f$.
     unsigned char* rho = (unsigned char*)calloc(hashLen + 1, sizeof(unsigned char));
     for(int i = 0; i < hashLen; i++) 
     {
@@ -323,6 +377,9 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
     }
     rho[hashLen] = '\0';
 
+    // Let \f$m = \mathrm{HashBytes}(|W|, rho, \mathrm{hashfcn}) \oplus W\f$, which is the bit-wise
+    // XOR of \f$m\f$ with the first \f$|W|\f$ octets of the pseudo-random bytes
+    // produced by HashBytes with seed {@code rho}.
     char* m = (char*)calloc(ciphertext.cipherWLength + 1, sizeof(char));
     unsigned char* hashedBytes = hashBytes(ciphertext.cipherWLength, rho, hashLen, publicParameters.hashFunction);
     for(int i = 0; i < ciphertext.cipherWLength; i++) 
@@ -331,8 +388,12 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
     }
     m[ciphertext.cipherWLength] = '\0';
 
+    // Let \f$t = \mathrm{hashfcn}(m)\f$ using the \f$hashfcn\f$ algorithm.
     unsigned char* t = (*(publicParameters.hashFunction.sha_hash))((unsigned char*) m, ciphertext.cipherWLength, NULL);
 
+    // Let \f$l = \mathrm{HashToRange}(rho || t, q, \mathrm{hashfcn}) using HashToRange
+    // on the \f$(2 * \mathrm{hashlen})\f$-octet concatenation of {@code rho} and
+    // \f$t\f$.
     unsigned char* concat = (unsigned char*)calloc(2*hashLen + 1, sizeof(unsigned char));
     for(int i = 0; i < hashLen; i++)
     {
@@ -352,6 +413,7 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
     free(hashedBytes);
     free(concat);
     
+    // Verify that \f$U = [l]P\f$.
     AffinePoint testPoint;
     status = affine_wNAFMultiply(&testPoint, l, publicParameters.pointP, publicParameters.ellipticCurve);
     if(status)
@@ -361,6 +423,7 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
         return status;
     }
 
+    // If this is the case, then the decrypted plaintext \f$m\f$ is returned.
     if(affine_isEquals(ciphertext.cipherU, testPoint))
     {
         affine_destroy(testPoint);
@@ -369,6 +432,7 @@ Status cryptid_decrypt(char **result, AffinePoint privateKey, CipherTextTuple ci
         return SUCCESS;
     }
 
+    // Otherwise, the ciphertext is rejected and no plaintext is returned.
     affine_destroy(testPoint);
     mpz_clear(l);
     free(m);
