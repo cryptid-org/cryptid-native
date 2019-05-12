59fed4b7c4ea6c7a7b4a4a70268e0dee33297d4d
Attila Bagossy <bagossyattila@outlook.com>
2019-05-12 18:18:56 +0200

    Renamed the build-static command to build

--- .task/cmd/build.js
+++ .task/cmd/build.js
@@ -2,7 +2,7 @@ const { buildStaticLibrary } = require('../common/build');
 
 
 module.exports = {
-    command: 'build-static [args...]',
+    command: 'build [args...]',
     desc: 'Builds a static library. Optional args will be passed to gcc.',
     handlerFactory(dependencies) {
         return function handler({ args }) {
