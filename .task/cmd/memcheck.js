151c67c3e46f3e56ba8bf85584b339e54396abaf
Attila Bagossy <bagossyattila@outlook.com>
2019-05-12 21:06:40 +0200

    Renamed the memory-check command to memcheck

--- .task/cmd/memcheck.js
+++ .task/cmd/memcheck.js
@@ -3,7 +3,7 @@ const { discoverAllComponents } = require('../common/util');
 
 
 module.exports = {
-    command: 'memory-check [components...]',
+    command: 'memcheck [components...]',
     desc: 'Runs the tests of the specified components with valgrind memory checking. If no components are set, all of them will be tested.',
     handlerFactory(dependencies) {
         return function handler({ components = discoverAllComponents(dependencies) }) {
