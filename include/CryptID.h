530884cbebed67c315bf17345f629b665083f5ba
Attila Bagossy <bagossyattila@outlook.com>
2020-01-23 17:21:31 +0100

    Rewritten the comments in CryptID.h to the underdocs cppreference style

--- include/CryptID.h
+++ include/CryptID.h
@@ -1,3 +1,10 @@
+/**
+ * # CryptID Identity-based Encryption
+ *
+ * ## Excerpt
+ *
+ * Routines implementing the Boneh-Franklin Identity-based Encryption scheme.
+ */
 #ifndef __CRYPTID_CRYPTID_H
 #define __CRYPTID_CRYPTID_H
 
@@ -11,53 +18,123 @@
 
 
 /**
+ * ---
+ * showing-off: How YAML attributes work.
+ * ---
+ *
+ * ## Excerpt
+ *
+ * Establishes a master secret and public parameters for a given security level.
+ *
+ * ## Description
+ *
  * Establishes a master secret and public parameters for a given security level. The master secret (as its name suggests)
  * should be kept secret, while the public parameters can be distributed among the clients.
- * @param securityLevel the desired security level
- * @param publicParameters Pointer in which the public parameters will be stored. If the return value is CRYPTID_SUCCESS
- *                         then it will point to correctly filled PublicParameters instance. Note, that the
- *                         q field must be mpz_init'd and thus mpz_clear'd be the caller.
- * @param masterSecret Out parameter which will hold the master secret. Must be mpz_init'd and mpz_clear'd by the caller.
- * @return CRYPTID_SUCCESS if everything went right
+ *
+ * ## Parameters
+ *
+ *   * securityLevel
+ *     * The desired security level.
+ *   * publicParameters
+ *     * Pointer in which the public parameters will be stored. If the return value is CRYPTID_SUCCESS
+ *       then it will point to correctly filled PublicParameters instance. Note, that the
+ *       q field must be mpz_init'd and thus mpz_clear'd be the caller.
+ *   * masterSecret
+ *     * Out parameter which will hold the master secret. Must be mpz_init'd and mpz_clear'd by the caller.
+ *
+ * ## Return Value
+ * 
+ * CRYPTID_SUCCESS if everything went right.
  */
 CryptidStatus cryptid_setup(const SecurityLevel securityLevel, PublicParameters* publicParameters, mpz_t masterSecret);
 
 /**
+ * ## Excerpt
+ *
  * Extracts the private key corresponding to a given identity string.
- * @param result Out parameter holding the private key (which is an AffinePoint). On CRYPTID_SUCCESS, it must be destroyed by 
- *               the caller. Initialization is done by this function.
- * @param identity the identity string we're extracting the private key for
- * @param identityLength the length of the identity string
- * @param publicParameters the IBE public parameters
- * @param masterSecret the master secret corresponding to the public parameters
- * @return CRYPTID_SUCCESS if everything went right
+ *
+ * ## Description
+ *
+ * Extracts the private key corresponding to a given identity string.
+ *
+ * ## Parameters
+ * 
+ *   * result
+ *     * Out parameter holding the private key (which is an AffinePoint). On CRYPTID_SUCCESS, it must be destroyed by 
+ *       the caller. Initialization is done by this function.
+ *   * identity
+ *     * The identity string we're extracting the private key for.
+ *   * identityLength
+ *     * The length of the identity string.
+ *   * publicParameters
+ *     * The IBE public parameters.
+ *   * masterSecret
+ *     * the master secret corresponding to the public parameters.
+ *
+ * ## Return Value
+ *
+ * CRYPTID_SUCCESS if everything went right.
  */
 CryptidStatus cryptid_extract(AffinePoint* result, const char *const identity, const size_t identityLength, 
                        const PublicParameters publicParameters, const mpz_t masterSecret);
 
 /**
+ * ## Excerpt
+ *
  * Encrypts a message with the given identity string.
- * @param result Out parameter storing the ciphertext. If the return value is CRYPTID_SUCCESS
- *               then it will point to an CipherTextTuple instance, that must be destroyed by the caller.
- *               Initialization is done by this function.
- * @param message the string to encrypt
- * @param messageLength the length of the message
- * @param identity the identity string to encrypt with
- * @param identityLength the length of the identity
- * @param publicParameters the IBE public parameters
- * @return CRYPTID_SUCCESS if everything went right
+ *
+ * ## Description
+ *
+ * Encrypts a message with the given identity string.
+ *
+ * ## Parameters
+ *
+ *   * result 
+ *     * Out parameter storing the ciphertext. If the return value is CRYPTID_SUCCESS
+ *       then it will point to an CipherTextTuple instance, that must be destroyed by the caller.
+ *       Initialization is done by this function.
+ *   * message
+ *     * The string to encrypt.
+ *   * messageLength
+ *     * The length of the message.
+ *   * identity
+ *     * The identity string to encrypt with.
+ *   * identityLength
+ *     * The length of the identity.
+ *   * publicParameters
+ *     * The IBE public parameters.
+ *
+ * ## Return Value
+ * 
+ * CRYPTID_SUCCESS if everything went right.
  */
 CryptidStatus cryptid_encrypt(CipherTextTuple *result, const char *const message, const size_t messageLength,
                        const char *const identity, const size_t identityLength, const PublicParameters publicParameters);
 
 /**
+ * ## Excerpt
+ *
  * Decrypts a previously encrypted message using the specified private key.
- * @param result Out parameter holding the message in plaintext. If the return value is CRYPTID_SUCCESS
- *               then it will point to a zero-terminated string, that must be destroyed by the caller. 
- * @param privateKey the private key to decrypt with
- * @param ciphertext the ciphertext to decrypt
- * @param publicParameters the IBE public parameters
- * @return CRYPTID_SUCCESS if everything went right
+ *
+ * ## Description
+ *
+ * Decrypts a previously encrypted message using the specified private key.
+ *
+ * ## Parameters
+ *
+ *   * result
+ *     * Out parameter holding the message in plaintext. If the return value is CRYPTID_SUCCESS
+ *       then it will point to a zero-terminated string, that must be destroyed by the caller.
+ *   * privateKey
+ *     * The private key to decrypt with.
+ *   * ciphertext
+ *     * The ciphertext to decrypt.
+ *   * publicParameters
+ *     * The IBE public parameters.
+ *
+ * ## Return Value
+ *
+ * CRYPTID_SUCCESS if everything went right.
  */
 CryptidStatus cryptid_decrypt(char **result, const AffinePoint privateKey, const CipherTextTuple ciphertext, 
                        const PublicParameters publicParameters);
