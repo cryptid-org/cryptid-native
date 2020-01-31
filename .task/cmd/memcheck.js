b957254e5318afe492c21a2d16e032a2686d1a8e
Attila Bagossy <bagossyattila@outlook.com>
2020-01-31 23:40:33 +0100

    Added the xmlOutput option to the memcheck task

--- .task/cmd/memcheck.js
+++ .task/cmd/memcheck.js
@@ -5,6 +5,13 @@ const { discoverAllComponents } = require('../common/util');
 module.exports = {
     command: 'memcheck [components...]',
     desc: 'Runs the tests of the specified components with valgrind memory checking. If no components are set, all of them will be tested.',
+    builder: {
+        xmlOutput: {
+            desc: 'Whether to write the memcheck output into an XML file (component results are written into separate files).',
+            type: 'boolean',
+            default: false
+        }
+    },
     handlerFactory(dependencies) {
         return function handler({ components = discoverAllComponents(dependencies) }) {
             runMemoryCheck(dependencies, components);
