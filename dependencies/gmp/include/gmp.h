c0efce502633b7a05f77e8624fd68d68ea7a61a3
Vécsi Ádám <vecsi96@gmail.com>
2020-02-19 22:58:44 +0100

    Added include guard and switched to ifdef from if defined.

--- dependencies/gmp/include/gmp.h
+++ dependencies/gmp/include/gmp.h
@@ -1,13 +1,17 @@
+#ifndef __CRYPTID_GMP_H
+#define __CRYPTID_GMP_H
 
 //TODO remove if cmake is ready
 #define __CRYPTID_GMP
 
-#if defined(__CRYPTID_GMP)
+#ifdef __CRYPTID_GMP
 
 #include "full-gmp.h"
 
 #elif defined(__CRYPTID_GMP_MINI)
 
 #include "mini-gmp.h"
+
+#endif
 
 #endif
