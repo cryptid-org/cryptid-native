4f6e4fba75e282166ea5c000f81622960f114c1d
Szilard <szilard@szurti.hu>
2020-03-03 18:07:21 +0100

    added basic test

--- test/src/ABE.test.c
+++ test/src/ABE.test.c
@@ -8,39 +8,83 @@
 
 #include "CryptID_ABE.h"
 
-TEST make_abe_work()
+TEST basic_abe_test(SecurityLevel securityLevel, char* message, AccessTree* accessTree, char** attributes)
 {
-    const char *message = "OGS";
-
     PublicKey_ABE* publickey = malloc(sizeof (PublicKey_ABE));
     MasterKey_ABE* masterkey = malloc(sizeof (MasterKey_ABE));
-    if (CRYPTID_SUCCESS != cryptid_setup_ABE(LOWEST, publickey, masterkey))
-    {
-        printf("Setup failed\n");
-        return -1;
-    }
+    CryptidStatus status = cryptid_setup_ABE(securityLevel, publickey, masterkey);
+    ASSERT_EQ(status, CRYPTID_SUCCESS);
 
-    char *attribute = "test";
-    AccessTree* children = childrenArray();
-    AccessTree* accessTree = createTree(1, children, attribute, strlen(attribute));
 
     EncryptedMessage_ABE* encrypted = malloc(sizeof (EncryptedMessage_ABE));
-    cryptid_encrypt_ABE(encrypted, message, strlen(message), publickey, accessTree);
+    status = cryptid_encrypt_ABE(encrypted, message, strlen(message), publickey, accessTree);
+    ASSERT_EQ(status, CRYPTID_SUCCESS);
 
     SecretKey_ABE* secretkey = malloc(sizeof (SecretKey_ABE));
-    char** attributes = attributeArray();
-    attributes[0] = "test";
-    cryptid_keygen_ABE(masterkey, attributes, secretkey);
 
-    char* result = malloc(sizeof(char)*100);
-    cryptid_decrypt_ABE(result, encrypted, secretkey);
+    status = cryptid_keygen_ABE(masterkey, attributes, secretkey);
+    ASSERT_EQ(status, CRYPTID_SUCCESS);
+
+    char* result;
+    status = cryptid_decrypt_ABE(&result, encrypted, secretkey);
+    ASSERT_EQ(status, CRYPTID_SUCCESS);
+    ASSERT_EQ(strcmp(result, message), 0);
+
+    free(result);
+    destroyPublicKey_ABE(publickey);
+    mpz_clear(masterkey->beta);
+    affine_destroy(masterkey->g_alpha);
+    free(masterkey);
+    destroyEncryptedMessage_ABE(encrypted);
+    destroySecretKey_ABE(secretkey);
 
     PASS();
 }
 
-SUITE(make_it_work)
+static void generateRandomString(char** output, size_t outputLength, char* alphabet, size_t alphabetSize)
 {
-    RUN_TEST(make_abe_work);
+    memset(*output, '\0', outputLength);
+    
+    for (size_t i = 0; i < outputLength - 1; i++)
+    {
+        size_t index = rand() % (alphabetSize - 1);
+
+        (*output)[i] = alphabet[index];
+    }
+
+    (*output)[outputLength - 1] = '\0';
+}
+
+SUITE(cryptid_abe_suite)
+{
+    char* defaultAlphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for(int i = 0; i < 4; i++)
+    {
+        int messageLength = rand() % (1000 - 1) + 1;
+        char* message = malloc(messageLength + 1);
+        generateRandomString(&message, messageLength + 1, defaultAlphabet, strlen(defaultAlphabet));
+
+        char** attributes = attributeArray();
+        int numChilds = 1;
+        AccessTree* accessTree = createTree((i%2)*(numChilds-1)+1, NULL, 0);
+        char* randomStr = malloc(20 + 1);
+        for(int a = 0; a < numChilds; a++)
+        {
+            generateRandomString(&randomStr, 20, defaultAlphabet, strlen(defaultAlphabet));
+            AccessTree* child = createTree(1, randomStr, strlen(randomStr));
+            accessTree->children[a] = child;
+            attributes[a] = randomStr;
+        }
+
+        RUN_TESTp(basic_abe_test, LOWEST, message, accessTree, attributes);
+
+        free(message);
+        destroyTree(accessTree);
+        free(attributes);
+
+        free(randomStr);
+    }
 }
 
 GREATEST_MAIN_DEFS();
@@ -49,7 +93,7 @@ int main(int argc, char **argv)
 {
     GREATEST_MAIN_BEGIN();
 
-    RUN_SUITE(make_it_work);
+    RUN_SUITE(cryptid_abe_suite);
 
     GREATEST_MAIN_END();
 }
