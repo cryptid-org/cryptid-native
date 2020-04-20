d9ea4ff78ef7bc00d3b889e64f088642eb588572
Attila Bagossy <bagossyattila@outlook.com>
2020-04-20 13:11:55 +0200

    Added include guard to PrimalityTest.h

--- include/util/PrimalityTest.h
+++ include/util/PrimalityTest.h
@@ -1,3 +1,6 @@
+#ifndef __CRYPTID_PRIMALITY_TEST_H
+#define __CRYPTID_PRIMALITY_TEST_H
+
 #include "gmp.h"
 #include "util/Validation.h"
 
@@ -7,3 +10,5 @@
  * @result CRYPTID_VALIDATION_SUCCESS if \f$p\f$ is a probable prime
  */
 CryptidValidationResult primalityTest_isProbablePrime(const mpz_t p);
+
+#endif
